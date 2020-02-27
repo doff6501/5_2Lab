5cc29de09357847145b1907cbed48e7c89b4cd31
doff6501 <61034864+doff6501@users.noreply.github.com>
2020-02-27 16:16:52 -0600

    Add files via upload

---
+++ Program5-1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        public enum WeekDays {
+            
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            List<String> tasklist = new List<string>();
+            
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task " + day);
+                string task = day + " S " + Console.ReadLine();
+                tasklist.Add (task);
+            }
+
+            foreach (string task in tasklist)
+            {
+                Console.WriteLine(task);
+            }
+
+
+        }
+    }
+}
